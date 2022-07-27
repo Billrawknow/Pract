5661f241970960b01080ef73484e795fca970337
Billrawknow <bildadronoh@gmail.com>
2022-07-27 05:09:11 +0300

    templates folder

--- pract.py
+++ pract.py
@@ -1,15 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app= Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>    CODING IS PURE MATHEMATICS ! </h1>'
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1> This is the About Page {username}</h1>'
-
-@app.route('/admin')
-def admin_page():
-    return '<h1> Admin Page </h1>'
+def hello_page():
+    return render_template('home.html')
