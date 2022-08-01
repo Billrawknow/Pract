20d9e05b034d049a1a0c10c2552cbf24b2df8830
Billrawknow <bildadronoh@gmail.com>
2022-08-01 09:18:00 +0300

    pract.py

--- pract.py
+++ pract.py
@@ -9,9 +9,9 @@ def home_page():
 
 @app.route('/admin')
 def admin_page():
-    items = [
+    item = [
         {'id': 1, 'date': 20/12/2022, 'name': 'Evaline', 'kilograms': '1200', 'amount': 9600},
         {'id': 2, 'date': 20/12/2022, 'name': 'Leonard', 'kilograms': '1300', 'amount': 10400},
         {'id': 3, 'date': 20/12/2022, 'name': 'Karen', 'kilograms': '1400', 'amount': 11200}
     ]
-    return render_template('admin.html', items=items)
+    return render_template('admin.html', items=item)
