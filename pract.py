a7c7a271fa8ae70d9279a32432d4cd4ca8b2ba36
Billrawknow <bildadronoh@gmail.com>
2022-08-04 09:39:57 +0300

    changes

--- pract.py
+++ pract.py
@@ -1,7 +1,20 @@
+from datetime import date
+from importlib import import_module
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pract.db'
+db=SQLAlchemy(app)
 
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    date = db.Column(db.DateTime)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    kilograms = db.Column(db.Integer(), nullable=False)
+    amount= db.Column(db.Integer(), nullable=False)
 @app.route('/')
 @app.route('/home')
 def home_page():
