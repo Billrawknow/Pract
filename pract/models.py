cff4d95beeb93bac5da688c69216774013b2756e
Billrawknow <bildadronoh@gmail.com>
2022-08-17 17:02:46 +0300

    changes made

--- pract/models.py
+++ pract/models.py
@@ -1,4 +1,13 @@
 from pract import db
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     date = db.Column(db.String(10))
@@ -6,7 +15,7 @@ class Item(db.Model):
     kilograms = db.Column(db.Integer(), nullable=False)
     amount= db.Column(db.Integer(), nullable=False)
 
-
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item {self.name}'
         
