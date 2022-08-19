dddedfb601a03167bb91a7f57f0b2e0eadc06e1c
Billrawknow <bildadronoh@gmail.com>
2022-08-19 12:09:54 +0300

    changes made

--- pract/__init__.py
+++ pract/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime,date
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pract.db'
+app.config['SECRET_KEY']='0f896b6af276bca4b27ec1c2'
+
 db=SQLAlchemy(app)
 
 from pract import routes
--- pract/routes.py
+++ pract/routes.py
@@ -1,6 +1,8 @@
 from pract import app
 from flask import render_template
-from pract.models import Item    
+from pract.models import Item
+from pract.forms import RegisterForm
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -10,3 +12,9 @@ def home_page():
 def admin_page():
     item = Item.query.all()
     return render_template('admin.html', items=item)
+
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
