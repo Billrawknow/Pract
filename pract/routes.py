d060653d261d92253762a7f618d91e28434fb56e
Billrawknow <bildadronoh@gmail.com>
2022-09-12 06:14:42 +0300

    danger message

--- pract/routes.py
+++ pract/routes.py
@@ -29,5 +29,5 @@ def register_page():
         return redirect(url_for('admin_page'))
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
-            flash(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}',category='danger')
     return render_template('register.html', form=form)
