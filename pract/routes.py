8851bb6bc1e74111abf8278666be9a37ddbc7ee8
Billrawknow <bildadronoh@gmail.com>
2022-10-05 05:28:35 +0300

    registration

--- pract/routes.py
+++ pract/routes.py
@@ -3,7 +3,7 @@ from pract import app
 from flask import render_template,redirect, url_for,flash
 
 from pract.models import Item,User
-from pract.forms import RegisterForm
+from pract.forms import RegisterForm,LoginForm
 from pract import db
 
 @app.route('/')
@@ -31,3 +31,8 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}',category='danger')
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
