2965e92bb59fa881926a6a9fea5fe14d91db04d4
Billrawknow <bildadronoh@gmail.com>
2022-10-05 05:26:59 +0300

    login.html

--- pract/__init__.py
+++ pract/__init__.py
@@ -1,7 +1,7 @@
 from datetime import date
 from importlib import import_module
-from flask import Flask, render_template,redirect, url_for, request
-
+from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime,date
 app= Flask(__name__)
